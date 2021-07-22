8c33638f4c1cce2cb338be245453574a97e1df84
dragly <d.paklin@gmail.com>
2021-07-22 20:03:08 +0300

    Lesson3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 
+from mainapp.models import Product, ProductCategory
+
 
 def index(request):
-    title = 'кателог'
+    title = 'катaлог'
 
-    links_menu = [
-        {'href': 'index', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
+    links_menu = ProductCategory.objects.all()
+
+    # links_menu = [
+    #     {'href': 'index', 'name': 'все'},
+    #     {'href': 'products_home', 'name': 'дом'},
+    #     {'href': 'products_office', 'name': 'офис'},
+    #     {'href': 'products_modern', 'name': 'модерн'},
+    #     {'href': 'products_classic', 'name': 'классика'},
+    # ]
+
+    products = Product.objects.all()[:4]
+
 
     context = {
         'title': title,
         'links_menu': links_menu,
+        'related_products': products,
     }
     return render(request, 'mainapp/products.html', context)
